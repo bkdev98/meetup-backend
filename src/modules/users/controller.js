bcb6987ba751da59747baed959388609aa0f31c5
bkdev98 <bkdev98@gmail.com>
2017-04-24 22:06:15 +0700

    create JWT token and add auth middleware to get meetups

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -1,4 +1,5 @@
 import User from './model';
+import { createToken } from './utils/createToken';
 
 export const loginWithAuth0 = async (req, res) => {
   const { ...args } = req.body;
@@ -7,7 +8,9 @@ export const loginWithAuth0 = async (req, res) => {
     const user = await User.create(args);
 
     return res.status(200).json({
+      success: true,
       user,
+      token: `JWT ${createToken(user)}`,
     });
   } catch (e) {
     return res.status(400).json({ error: true, errorMessage: 'Something wrong with auth' });
